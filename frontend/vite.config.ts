e2753884185ab0be41786722f1b07f0cc8645793
Anmol Srivastava <srianmol04@gmail.com>
2026-08-09 14:09:29 +0000

    fix deployment and backend data

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   server: {
@@ -16,7 +15,13 @@ const config = defineConfig({
     },
   },
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+    nitro(),
+  ],
 })
 
 export default config
